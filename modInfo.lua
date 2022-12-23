cad04ba0831b17e275144305536f7e63d6f40ebf
earmuffins <ikren04@gmail.com>
2022-12-23 02:29:35 -0500

    Update modInfo.lua

--- modInfo.lua
+++ modInfo.lua
@@ -1,7 +1,7 @@
 local modInfo = {
     name = "Automated Mining",
     description = "Introduces mines, a workstation for renewable resources.\n\nFound a bug? Have a suggestion? Please notify me on Discord at earmuffs#3820. Thanks!",
-    version = "0.0.2",
+    version = "0.0.3",
     type = "world",
     preview = "assets/preview.jpg",
     developer = "earmuffs"
